85c4b2eec55abc73f6565dc31c4ae5d714c5587f
Christian Staib <staib.christian@hotmail.de>
2024-06-06 17:18:15 +0200

    added path checks

--- scripts/fmi_float_to_int.py
+++ scripts/fmi_float_to_int.py
@@ -1,11 +1,14 @@
-from math import ceil
+import math
 import click
 from tqdm import tqdm
 
 @click.command()
-@click.option('--input_file')
-@click.option('--output_file')
+@click.option('--input_file', required=True, type=click.Path(exists=True, dir_okay=False), help='Input file path.')
+@click.option('--output_file', required=True, type=click.Path(dir_okay=False, writable=True), help='Output file path.')
 def process_file(input_file, output_file):
+    """
+    Processes the input file and writes to the output file.
+    """
     with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
         while True:
             line = infile.readline().strip()
@@ -18,19 +21,16 @@ def process_file(input_file, output_file):
         outfile.write(num_vertices + "\n")
         outfile.write(num_edges + "\n")
 
-        for _ in tqdm(range(int(num_vertices))):
+        for _ in tqdm(range(int(num_vertices)), desc="Processing vertices"):
             outfile.write(infile.readline())
         
-
-        for _ in tqdm(range(int(num_edges))):
+        for _ in tqdm(range(int(num_edges)), desc="Processing edges"):
             line = infile.readline()
-
-            new_line = list()
+            new_line = []
 
             for number in line.split():
                 if "." in number:
-                    number = str(int(ceil(float(number))))
-
+                    number = str(math.ceil(float(number)))
                 new_line.append(number)
 
             outfile.write(' '.join(new_line) + '\n')
