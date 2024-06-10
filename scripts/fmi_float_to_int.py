c6552bce76fc9ba90193f5d4092e25273525a786
Christian Staib <staib.christian@hotmail.de>
2024-06-10 11:55:15 +0200

    added try catch

--- scripts/fmi_float_to_int.py
+++ scripts/fmi_float_to_int.py
@@ -29,28 +29,33 @@ def process_file(input_file, output_file, divider):
     """
     Processes the input file and writes to the output file.
     """
-    with open(input_file, "r") as infile, open(output_file, "w") as outfile:
-        while True:
-            line = infile.readline().strip()
-            outfile.write(line + "\n")
-            if not line.startswith("#"):
-                break
+    try:
+        with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+            while True:
+                line = infile.readline().strip()
+                outfile.write(line + "\n")
+                if not line.startswith("#"):
+                    break
 
-        num_vertices = infile.readline().strip()
-        num_edges = infile.readline().strip()
-        outfile.write(num_vertices + "\n")
-        outfile.write(num_edges + "\n")
+            num_vertices = infile.readline().strip()
+            num_edges = infile.readline().strip()
+            outfile.write(num_vertices + "\n")
+            outfile.write(num_edges + "\n")
 
-        for _ in tqdm(range(int(num_vertices)), desc="Processing vertices"):
-            outfile.write(infile.readline())
+            for _ in tqdm(
+                range(int(num_vertices)), desc="Processing vertices", leave=False
+            ):
+                outfile.write(infile.readline())
 
-        for _ in tqdm(range(int(num_edges)), desc="Processing edges"):
-            numbers = infile.readline().split()
+            for _ in tqdm(range(int(num_edges)), desc="Processing edges", leave=False):
+                numbers = infile.readline().split()
 
-            # process edge weights
-            numbers[2] = str(math.ceil(float(numbers[2]) / divider))
+                # process edge weights
+                numbers[2] = str(math.ceil(float(numbers[2]) / divider))
 
-            outfile.write(" ".join(numbers) + "\n")
+                outfile.write(" ".join(numbers) + "\n")
+    except:
+        print(f"error in {input_file}")
 
 
 if __name__ == "__main__":
